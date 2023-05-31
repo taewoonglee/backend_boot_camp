9058d3cb8e7ae93cc5caa39fac9b0c6bd6386907
xodndlee <dnd5436@gmail.com>
2023-05-31 18:17:10 +0900

    2023-05-31-18:17

--- MyThread.java
+++ MyThread.java
@@ -4,28 +4,31 @@ public class MyThread extends Thread{
     private int time;
     private int id;
     private Person person;
-    private static int count;
+    //private static int count;
 
     public MyThread(int time, int id, Person person) {
         this.time = time;
         this.id = id;
         this.person=person;
-        count++;
+        //count++;
     }
     @Override
     public void run(){
-        for (int i = 0; i < 5; i++) {
-            person.setName("Name"+id);
+
+
+            person.setNow_state(0);
             try {
                 Thread.sleep(time);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println(id);
-            System.out.println(person.getName());
+            person.setNow_state(1);
+            if(person.getName()=="scv")
+            {
+                Main.scv_is_availble=true;
+            }
+            //여기서 getname==scv일때 또다른 thread를 실
+//        person.getName()==scv
 
-        }
-        count--;
-        System.out.println("대기순번 : " +MyThread.count );
     }
 }
